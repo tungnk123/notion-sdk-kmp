2d1c5d8e87965c39eb61a34702ec7bb850013ba2
tungnk123 <doanthanhtungnk123@gmail.com>
2025-09-27 12:31:17 +0700

    Add some dependencies and update build.gradle.kts

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.vanniktech.mavenPublish)
 }
 
@@ -11,39 +12,43 @@ group = "io.github.kotlin"
 version = "1.0.0"
 
 kotlin {
+    applyDefaultHierarchyTemplate()
+
     jvm()
     androidTarget {
         publishLibraryVariants("release")
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
+        compilerOptions { jvmTarget.set(JvmTarget.JVM_11) }
     }
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
+    js(IR) { browser() }
+    iosX64(); iosArm64(); iosSimulatorArm64()
     linuxX64()
 
     sourceSets {
         val commonMain by getting {
             dependencies {
-                //put your multiplatform dependencies here
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.kotlinx.serialization.json)
+                implementation(libs.kotlinx.datetime)
+                implementation(libs.bundles.ktor.common)
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(libs.kotlin.test)
+                implementation(libs.ktor.client.mock)
             }
         }
+        val jvmMain by getting { dependencies { implementation(libs.ktor.client.cio) } }
+        val androidMain by getting { dependencies { implementation(libs.ktor.client.android) } }
+        val jsMain by getting { dependencies { implementation(libs.ktor.client.js) } }
+        val iosMain by getting { dependencies { implementation(libs.ktor.client.darwin) } }
     }
 }
 
 android {
     namespace = "org.jetbrains.kotlinx.multiplatform.library.template"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
+    defaultConfig { minSdk = libs.versions.android.minSdk.get().toInt() }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -52,34 +57,15 @@ android {
 
 mavenPublishing {
     publishToMavenCentral()
-
     signAllPublications()
-
     coordinates(group.toString(), "library", version.toString())
-
     pom {
         name = "My library"
         description = "A library."
         inceptionYear = "2024"
         url = "https://github.com/kotlin/multiplatform-library-template/"
-        licenses {
-            license {
-                name = "XXX"
-                url = "YYY"
-                distribution = "ZZZ"
-            }
-        }
-        developers {
-            developer {
-                id = "XXX"
-                name = "YYY"
-                url = "ZZZ"
-            }
-        }
-        scm {
-            url = "XXX"
-            connection = "YYY"
-            developerConnection = "ZZZ"
-        }
+        licenses { license { name = "XXX"; url = "YYY"; distribution = "ZZZ" } }
+        developers { developer { id = "XXX"; name = "YYY"; url = "ZZZ" } }
+        scm { url = "XXX"; connection = "YYY"; developerConnection = "ZZZ" }
     }
 }
