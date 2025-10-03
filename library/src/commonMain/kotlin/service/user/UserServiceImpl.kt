f742a02b89ca733d801817fee03e551d89c76aa7
tungnk123 <doanthanhtungnk123@gmail.com>
2025-10-04 00:13:24 +0700

    Update UserServiceImpl class

--- library/src/commonMain/kotlin/service/user/UserServiceImpl.kt
+++ library/src/commonMain/kotlin/service/user/UserServiceImpl.kt
@@ -3,15 +3,22 @@ package service.user
 import core.data.model.internal.dto.user.UserDto
 import core.data.model.internal.response.ResultsResponseDto
 import http.NotionHttp
-import io.ktor.client.request.*
+
+private object Routes {
+    private const val USERS = "users"
+    fun list() = USERS
+    fun retrieve(id: String) = "$USERS/$id"
+    fun me() = "$USERS/me"
+}
 
 class UserServiceImpl(private val http: NotionHttp) : UserService {
-    override suspend fun list(startCursor: String?, pageSize: Int?): ResultsResponseDto<UserDto> = http.get("users") {
-        if (startCursor != null) parameter("start_cursor", startCursor)
-        if (pageSize != null) parameter("page_size", pageSize)
-    }
+    override suspend fun list(startCursor: String?, pageSize: Int?): ResultsResponseDto<UserDto> =
+        http.get(Routes.list()) {
+            if (startCursor != null) url.parameters.append("start_cursor", startCursor)
+            if (pageSize != null) url.parameters.append("page_size", pageSize.toString())
+        }
 
-    override suspend fun retrieve(id: String): UserDto = http.get("users/$id")
+    override suspend fun retrieve(id: String): UserDto = http.get(Routes.retrieve(id))
 
-    override suspend fun me(): UserDto = http.get("users/me")
+    override suspend fun me(): UserDto = http.get(Routes.me())
 }
