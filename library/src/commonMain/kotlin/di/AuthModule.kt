a12999b7e1fdbe8eec3b693d70ca763679bc8789
tungnk123 <doanthanhtungnk123@gmail.com>
2025-10-09 22:59:00 +0700

    Add AuthModule for dependency injection

--- library/src/commonMain/kotlin/di/AuthModule.kt
+++ library/src/commonMain/kotlin/di/AuthModule.kt
@@ -1,18 +1,33 @@
 package di
 
-import auth.*
+import auth.OAuthTokenProvider
 import auth.repository.AuthRepository
+import auth.repository.AuthRepositoryFactory
 import auth.service.AuthService
 import auth.service.AuthServiceImpl
+import auth.tokenstorage.InMemoryMultiTokenStorage
+import auth.tokenstorage.InMemoryTokenStorage
+import auth.tokenstorage.MultiTokenStorage
+import auth.tokenstorage.TokenStorage
 import io.ktor.client.*
+import notion.NotionSessionManager
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
 
 val authModule = module {
     single { HttpClient() }
     single<AuthService> { AuthServiceImpl(get()) }
     single<TokenStorage> { InMemoryTokenStorage() }
+    single<MultiTokenStorage> { InMemoryMultiTokenStorage() }
     single { (clientId: String, clientSecret: String) ->
         AuthRepository(service = get(), clientId = clientId, clientSecret = clientSecret, storage = get())
     }
     factory { (repo: AuthRepository) -> OAuthTokenProvider(repo) }
+
+    factory { (clientId: String, clientSecret: String) ->
+        AuthRepositoryFactory(service = get(), clientId = clientId, clientSecret = clientSecret, multi = get())
+    }
+    factory { (clientId: String, clientSecret: String) ->
+        NotionSessionManager(factory = get { parametersOf(clientId, clientSecret) }, storage = get())
+    }
 }
